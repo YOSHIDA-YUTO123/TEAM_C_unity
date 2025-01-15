1b0fd0d9a295c47f0402afffa0a9e1d16037003a
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2025-01-15 10:45:41 +0900

    スクリプトを作成

---
+++ Assets/Scripts/sceneManagerEX.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagerEX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {// ƒQ[ƒ€ƒV[ƒ“‚ğ“Ç‚İ‚Ş
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
