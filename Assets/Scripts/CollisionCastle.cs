40edeb17b50f70922f3d50123dcb27a9d7924b6a
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2025-01-22 11:42:06 +0900

    アセット

--- Assets/Scripts/CollisionCastle.cs
+++ Assets/Scripts/CollisionCastle.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CollisionCastle : MonoBehaviour
 {
+    // 城と敵の当たり判定
+    public int CastleLife = 100; // 城体力
+    public int nDamege = 2;      // ダメージ量
+
+    public GameObject effectPrefab;// エフェクトプレハブ用
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,35 @@ public class CollisionCastle : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 敵との衝突
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            // 体力を減らす
+            CastleLife -= nDamege;
+
+            // 当たったら
+            Destroy(other.gameObject);
+
+            if (CastleLife > 0)
+            {// 体力が0以上
+                // エフェクト
+                GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+
+                // エフェクトを4秒後に画面から消す
+                Destroy(effect, 4.0f);
+            }
+
+            if (CastleLife <= 0)
+            {// 体力が0以下
+                // 城なくなる
+                gameObject.SetActive(false);
+            }
+
+        }
+    }
+
 }
+
