bd7028a9849e2d88daa2c5b72c4eb0be591455c5
Nagao <241314@st.yoshida-g.ac.jp>
2025-01-22 09:16:40 +0900

    弾に攻撃力の変数を付加

--- Assets/Scripts/BulletControl1.cs
+++ Assets/Scripts/BulletControl1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BulletControl1 : MonoBehaviour
 {
     public float BulletSpeed = 0.1f;
+    public int Damege = 10;
     // Start is called before the first frame update
     void Start()
     {
