9c7f88d98e48392384d5f54ce7683c726f811cc2
YUTO YOSHIDA <241327@st.yoshida-g.ac.jp>
2025-01-29 10:44:56 +0900

    敵のスポナーせいさく

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -10,14 +10,14 @@ public class RockController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.fallSpeed = 0.1f * Random.value;
+        this.fallSpeed = 0.05f * Random.value;
         this.rotSpeed = 5.0f + 3.0f * Random.value;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-fallSpeed, 0, 0, Space.World);
+        transform.Translate(0, -fallSpeed, 0, Space.World);
         transform.Rotate(0, 0, rotSpeed);
 
         if (transform.position.x < -8.5f || transform.position.y > 9.0f || transform.position.y < -9.0f)
