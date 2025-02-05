0936f75d28b611a958bcd268adc912476ab84139
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2025-02-05 09:39:14 +0900

    エフェクト追加

--- Assets/Scripts/CollisionBulletController.cs
+++ Assets/Scripts/CollisionBulletController.cs
@@ -11,6 +11,10 @@ public class CollisionBulletController : MonoBehaviour
     //public GameObject bulletPrefab;
     public GameObject SpeedItem;
 
+    public GameObject effectPrefab;// エフェクトプレハブ用
+
+    public float fTimevalue = 2.0f; // 有効時間
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +41,12 @@ public class CollisionBulletController : MonoBehaviour
 
             if (EnemyHP <= 0)
             {
+                // エフェクト
+                GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+
+                // エフェクトを2秒後に画面から消す
+                Destroy(effect, fTimevalue);
+
                 gameObject.SetActive(false);
                 Destroy(gameObject);
 
